babc64850a33c1eca95a625168e3a7d76a0687ca
KarloFrontEnd <96472437+KarloFrontEnd@users.noreply.github.com>
2022-01-06 21:20:17 +0100

    Add files via upload

---
+++ PieChart.py
@@ -0,0 +1,13 @@
+from matplotlib import pyplot as plt
+
+plt.style.use("fivethirtyeight")
+
+slices = [1439323776, 1380004385, 331002651, 273523615, 220892340, 212559417]
+labels = ['China', 'India', 'United States', 'Indonesia', 'Pakistan', 'Brazil']
+
+
+plt.pie(slices, labels=labels, wedgeprops={'edgecolor': 'black'})
+
+plt.title("Top 5 Countries Population")
+plt.tight_layout()
+plt.show()
