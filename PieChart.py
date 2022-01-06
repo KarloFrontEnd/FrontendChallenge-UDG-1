d1ca60d5fa8f148747e26bb2004e387785396b0c
KarloFrontEnd <96472437+KarloFrontEnd@users.noreply.github.com>
2022-01-07 00:45:51 +0100

    Update PieChart.py

--- PieChart.py
+++ PieChart.py
@@ -6,7 +6,7 @@ slices = [1439323776, 1380004385, 331002651, 273523615, 220892340, 212559417]
 labels = ['China', 'India', 'United States', 'Indonesia', 'Pakistan', 'Brazil']
 
 
-plt.pie(slices, labels=labels, wedgeprops={'edgecolor': 'black'})
+plt.pie(slices, labels=labels, shadow=True, autopct='%1.1f%%', wedgeprops={'edgecolor': 'black'})
 
 plt.title("Top 5 Countries Population")
 plt.tight_layout()
